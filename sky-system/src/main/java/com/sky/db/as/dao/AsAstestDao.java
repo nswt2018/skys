8709c0973af25f623c9fab7ea828af18f48497ca
liuxiaosan333 <43360349+liuxiaosan333@users.noreply.github.com>
2019-01-28 16:35:57 +0800

    liuxiaosan333

--- sky-system/src/main/java/com/sky/db/as/dao/AsAstestDao.java
+++
@@ -1,9 +0,0 @@
-package com.sky.db.as.dao;
-
-import com.sky.db.as.model.AsAstest;
-
-
-import tk.mybatis.mapper.common.Mapper;
-
-public interface AsAstestDao extends Mapper<AsAstest>{
-} 
--- sky-system/src/main/java/com/sky/db/as/model/AsAstest.java
+++
@@ -1,34 +0,0 @@
-package com.sky.db.as.model;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.apache.commons.lang3.builder.ToStringExclude;
-import org.apache.ibatis.type.Alias;
-
-@Alias("AsAstest")
-@Table(name="astest")
-public class AsAstest {
-    @Id
-    private Integer astestKey;
-    private String astestOne;
-    
-    public Integer getAstestKey() {
-		return astestKey;
-	}
-	public void setAstestKey(Integer astestKey) {
-		this.astestKey = astestKey;
-	}
-	public String getAstestOne(){
-        return this.astestOne;
-    }
-    public void setAstestOne(String astestOne){
-        this.astestOne = astestOne;
-    }
-
-}
-
